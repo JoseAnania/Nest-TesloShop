13c5c5b8e2a827fbd032d19d856d038d61ec9e0d
janania <joseanania47@gmail.com>
2023-02-26 17:40:07 -0300

    commit 26/02/2023

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    
+    // importamos el servicio que nos permite utilizar las variables de entorno (".env")
+    ConfigModule.forRoot(),
+
+    // importamos el servicio de conexión a la BD (Postgres) que se encuentra en las variables de entorno (".env")
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  
+  ],
   controllers: [],
   providers: [],
 })
